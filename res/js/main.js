d9ae85a80db6fd78483c9d323cad92b4c55c374e
Natalka3 <115136395+Natyv16@users.noreply.github.com>
2022-10-20 09:08:58 +0200

    Update main.js

--- res/js/main.js
+++ res/js/main.js
@@ -2,19 +2,4 @@ const send = document.getElementById("send");
 const content = document.getElementById("content");
 const userInput = document.getElementById("userInput");
 
-const changeColor = (color) => {
-    const userValue = userInput.value;
-  content.style.backgroundColor = color;
-}
 
-send.onclick = () => {
-    changeColor(userInput.value);
-}
-
-content.onmouseover = () => {
-    changeColor(userInput.value);
-}
-
-window.onload = () => {
-    changeColor ("pink")
-}
